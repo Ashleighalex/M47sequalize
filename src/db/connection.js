bd0cbef15558be46d06ca6eb3c963a8307476179
Ashleighalex <ashleighalexandra199824@gmail.com>
2023-05-17 12:27:52 +0100

    sequalize completed submission

---
+++ src/db/connection.js
@@ -0,0 +1,14 @@
+// import sequilize
+const { Sequelize } = require("sequelize")
+
+
+// initalise a new instance of the class sequelize
+const connection = new Sequelize(process.env.MYSQL_URI)
+
+
+// checks the connection URI to make sure there is a existing database to connect to
+connection.authenticate()
+
+console.log("DB connection is working")
+
+module.exports = connection
